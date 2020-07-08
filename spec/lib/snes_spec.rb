58b96bc9b7c9a71183c881d92b7906004f9bd85f
michebble <32097294+michebble@users.noreply.github.com>
2020-07-08 21:19:21 +1000

    Update Snes to implement Mister

--- spec/lib/snes_spec.rb
+++ spec/lib/snes_spec.rb
@@ -11,6 +11,15 @@ describe Snes do
     end
   end
 
+  describe '.to_mister_format' do
+    context 'when a valid game genie code is provided' do
+      it 'converts it to raw', :aggregate_failures do
+        expect(subject.to_mister_format('DDD1-8D94')).to eq('00 00 00 00 61 0A C4 00 00 00 00 00 00 00 00 00')
+        expect(subject.to_mister_format('D998-776E')).to eq('00 00 00 00 BE 5C C3 00 00 00 00 00 05 00 00 00')
+      end
+    end
+  end
+
   xdescribe '.to_bit_string' do
     context 'when a valid game genie code is provided' do
       it 'converts it to bitstring', :aggregate_failures do
