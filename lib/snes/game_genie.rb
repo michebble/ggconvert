17e1daf1fd4b937e212eed90419783dde14c2989
michebble <32097294+michebble@users.noreply.github.com>
2020-07-09 20:07:03 +1000

    Change Snes::GameGenie behaviour
    Removes abilty to output the mister format, instead will now decrypt to
    raw format. This is because the two classes were too coupled.

--- lib/snes/game_genie.rb
+++ lib/snes/game_genie.rb
@@ -4,36 +4,22 @@ module Snes
   class GameGenie
     ALPHABET = %w[D F 4 7 0 9 1 5 6 B C 8 A 2 3 E].freeze
 
-    def initialize(formatter: Mister)
-      @formatter = formatter
+    def initialize(code:)
+      @code = code
     end
 
-    def to_mister_format(code)
-      result = decrypt(code)
-      @formatter.new(**result).format
+    def decrypt
+      {
+        address: address_value,
+        replace: replace_value,
+        compare: nil
+      }
     end
 
     private
 
-    def to_raw(code)
-      result = decrypt(code)
-      [
-        result[:address],
-        result[:replace],
-        result[:compare]
-      ].compact.join(':')
-    end
-
-    def decrypt(code)
-      bit_string = to_bit_string(code)
-      {
-        address: address_value(bit_string),
-        replace: replace_value(bit_string)
-      }
-    end
-
-    def to_bit_string(code)
-      code
+    def bit_string
+      @bit_string ||= code
         .tr('-', '')
         .split('')
         .reduce(0) do |bit_string, letter|
@@ -43,12 +29,12 @@ module Snes
         end
     end
 
-    def replace_value(bit_string)
+    def replace_value
       value = (bit_string >> 24) & 0xFF
       sprintf('%<value>02d', value: value)
     end
 
-    def address_value(bit_string)
+    def address_value
       address = ((bit_string >> 10) & 0xC) | ((bit_string >> 10) & 0x3)
 
       address <<= 4
@@ -67,5 +53,7 @@ module Snes
       address |= ((bit_string >> 6) & 0xC) | ((bit_string >> 6) & 0x3)
       address.to_s(16).upcase
     end
+
+    attr_reader :code
   end
 end
--- spec/lib/snes/game_genie_spec.rb
+++ spec/lib/snes/game_genie_spec.rb
@@ -1,32 +1,22 @@
 require_relative '../../../lib/snes/game_genie'
 
 describe Snes::GameGenie do
-  let(:subject) { described_class.new }
+  let(:subject) {  described_class.new(code: game_genie_code) }
+  let(:game_genie_code) { 'D998-776E' }
 
-  describe '.to_mister_format' do
-    context 'when a valid game genie code is provided' do
-      it 'converts it to the mister format', :aggregate_failures do
-        expect(subject.to_mister_format('DDD1-8D94')).to eq '00 00 00 00 61 0A C4 00 00 00 00 00 00 00 00 00'
-        expect(subject.to_mister_format('D998-776E')).to eq '00 00 00 00 BE 5C C3 00 00 00 00 00 05 00 00 00'
-      end
-    end
-  end
+  describe '.decrypt' do
+    let(:raw_code) { { address: 'C35CBE', replace: '05', compare: nil } }
 
-  xdescribe '.to_raw' do
-    context 'when a valid game genie code is provided' do
-      it 'converts it to raw', :aggregate_failures do
-        expect(subject.to_raw('DDD1-8D94')).to eq('C40A61:00')
-        expect(subject.to_raw('D998-776E')).to eq('C35CBE:05')
-      end
+    it 'converts it to raw' do
+      expect(subject.decrypt).to eq(raw_code)
     end
   end
 
   xdescribe '.to_bit_string' do
-    context 'when a valid game genie code is provided' do
-      it 'converts it to bitstring', :aggregate_failures do
-        expect(subject.to_bit_string('DDD1-8D94')).to eq(438_354)
-        expect(subject.to_bit_string('D998-776E')).to eq(89_863_055)
-      end
+    let(:bit_string) { 89_863_055 }
+
+    it 'converts it to bitstring' do
+      expect(subject.to_bit_string).to eq(bit_string)
     end
   end
 end
